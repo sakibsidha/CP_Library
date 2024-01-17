f417c6ed56a1717a1706759bb1eef64babec4b7e
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-01-17 11:34:53 +0600

    Create gen.cpp

---
+++ my_setup/gen.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int rand(int a, int b){
+	return a + rand() % (b - a + 1);
+}
+
+int main(int argc, char* argv[]){
+	srand(atoi(argv[1]));
+	// gen code here, run by ./gen (n)
+	
+}
