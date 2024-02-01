1e5bd7c220aed97b5b7819b4ce4141862e82a47e
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-02-01 19:43:14 +0600

    Update MatExpo.cpp

--- Topics/Math/MatExpo.cpp
+++ Topics/Math/MatExpo.cpp
@@ -1,50 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
-//#include "debug.h"
-
-const int MOD = 1e9+7;
-
-class matExpo{
+const long long mod = 1e9+7;
+#define Matrix vector<vector<T>>
+class MatExpo{
 public:
 	template <typename T> 
-	vector<vector<T>> matMult(vector<vector<T>> A, vector<vector<T>> B){
-		vector<vector<T>> C(A.size(), vector<T>(B[0].size(), 0));
+	Matrix mult(Matrix A, Matrix B){
+		Matrix res(A.size(), vector<T>(B[0].size(), 0));
 		for(int i = 0; i < A.size(); ++i){
-			for(int j = 0; j < B.size(); ++j){
+			for(int j = 0; j < B[0].size(); ++j){
 				for(int k = 0; k < B.size(); ++k){
-					C[i][j] += A[i][k] * B[k][j];
+					res[i][j] += A[i][k] * B[k][j];
+					res[i][j] %= mod;
 				}
 			}
 		}
-		return C;
+		return res;
+	}
+	template <typename T>
+	void solution(T n){
+		// identity matrix
+		Matrix base = {
+			{1, 0},
+			{0, 1}
+		};
+		// matrix to be exponentiated
+		Matrix mat = {
+			{0, 1},
+			{1, 1}
+		};
+		// exponentiation; base will be the answer
+		for(; n; n >>= 1, mat = mult(mat, mat)){
+			if(n&1) base = mult(base, mat);
+		}
+		// now we can keep the base values of the reccurrence in a matrix
+		Matrix rec = {{0},{1}};
+		Matrix ans = mult(base, rec);
+		cout << ans[0][0] << endl;
 	}
 };
 
 void solve(){
-	matExpo obito;
-	vector<vector<int>> A(3, vector<int>(2, 0));
-	vector<vector<int>> B(2, vector<int>(2, 0));
-	for(int i = 0; i < A.size(); ++i){
-		for(int j = 0; j < A[0].size(); ++j){
-			cin >> A[i][j];
-		}
-	}
-	for(int i = 0; i < B.size(); ++i){
-		for(int j = 0; j < B[0].size(); ++j){
-			cin >> B[i][j];
-		}
-	}
-	//debug() << db(A) << db(B);
-	vector<vector<int>> ans = obito.matMult(A,B);
-	//debug() << db(ans);
+	long long n; cin >> n;
+	MatExpo m;
+	m.solution(n);
 }
 
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	// int tc; cin >> tc;
-	// while(tc--)
+	//int tc; cin >> tc;
+	//while(tc--)
 		solve();
 
 	return 0;
