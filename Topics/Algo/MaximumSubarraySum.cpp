eca7e8ecd289e25ac57dbe4494ec203b7b7bfa3b
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-02-03 11:29:17 +0600

    Update MaximumSubarraySum.cpp

--- Topics/Algo/MaximumSubarraySum.cpp
+++ Topics/Algo/MaximumSubarraySum.cpp
@@ -1,1 +1,22 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+ 
+	long long n; cin >> n;
+	vector<long long> v(n);
+	for(int i = 0; i < n; ++i){
+		cin >> v[i];
+	}
+	long long ans = LLONG_MIN;
+	long long max_so_far = 0; // max subarray sum ending at i
+	for(int i = 0; i < n; ++i){
+		max_so_far = max(v[i], v[i] + max_so_far);
+		ans = max(ans, max_so_far);
+	}
+	cout << ans << "\n";
+ 
+	return 0;
+}
