a1536aa0995b2c40f664ce7227c096f8137c3e4b
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-02-21 19:17:02 +0600

    Create EGCD.cpp

---
+++ Topics/Math/EGCD.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+// ax + by = gcd(a,b)
+
+int egcd(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = egcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+void solve(){
+	int x, y;
+	int GCD = egcd(4,7,x,y);
+	cout << "GCD of 4 and 7 is: " << GCD << endl;
+	cout << "x: " << x << " & y: " << y << endl;
+	
+	/* if gcd(a,b) is 1, that is they are co-prime, then x will be the inverse modulo of 
+	 * a in b modular field. 
+	 * let,
+	 * 1/a (mod m) = x     [x is the inverse modulo of a]
+	 * =>  ax = 1 (mod m) 
+	 * => ax + my = 1 (mod m)
+	*/
+	
+	cout << "Inverse Modulo of 4 in 7 mod field: " << x << endl;
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	//int tc; cin >> tc;
+	//while(tc--)
+		solve();
+
+	return 0;
+}
