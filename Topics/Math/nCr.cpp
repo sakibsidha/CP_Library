9fccf26998c455e4c6a104fd03950fe5fb58696f
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-02-21 20:45:38 +0600

    Update nCr.cpp

--- Topics/Math/nCr.cpp
+++ Topics/Math/nCr.cpp
@@ -5,7 +5,7 @@ using ll = long long;
 // prime mod
 const ll MAXX = 2*1e6;
 const ll mod = 1e9+7;
-ll fact[MAXX], inv[MAXX]; // // fact and inverse fact array
+ll fact[MAXX], inv[MAXX]; // fact and inverse fact array
 
 ll Pow(ll a, ll b){ ll ans = 1; while(b){ if(b&1) ans = (ans*a)%mod; a = (a*a)%mod; b >>= 1; } return ans; }
 
