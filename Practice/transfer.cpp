555a83bd452d414607aa9c1dce3c81b49e3e4c58
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-04-23 13:54:04 +0600

    Update transfer.cpp

--- Practice/transfer.cpp
+++ Practice/transfer.cpp
@@ -1,53 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e6+10;
-int gpf[N+1], lpf[N+1], divsum[N+1], divcount[N+1],
-    dist_pf[N+1], tot_pf[N+1];
-bool prime[N+1];
+#include "debug.h"
 
-void sieve(){
-    for(int i = 1; i <= N; ++i){
-        for(int j = i; j <= N; j += i){
-            divcount[j]++;
-            divsum[j] += i;
-        }
-    }
+void solve(){
+	int n; cin >> n;
+	int a[n][n];
+	for(int i = 0; i < n; ++i){
+		for(int j = 0; j < n; ++j){
+			cin >> a[i][j];
+		}
+	}
+	for(int i = 0; i < n; ++i){
+		for(int j = i + 1; j < n; ++j){
+			int f = 0;
+			for(int k = 0; k < n; ++k){
+				if(k == i || k == j) continue;
+				else{
+					// debug() << db(i) << db(j) << db(k);
+					cout << "hi";
+					if(a[i][k] + a[k][j] == a[i][j]){
+						f = 1;
+						break;
+					}
+				}
+			}
+			if(f == 0){
+				cout << i + 1 << " " << j + 1 << "\n";
+			}
+		}
+	}
 }
 
-void another_sieve(){
-    for(int i = 2; i*i <= N; ++i){
-        if(prime[i] == false){
-            gpf[i] = i;
-            lpf[i] = i;
-            for(int j = 2*i; j <= N; j += i){
-                if(prime[j] == true){
-                    gpf[j] = i;
-                }
-                else{
-                    gpf[j] = i;
-                    lpf[j] = i;
-                    prime[j] = true;
-                }
-            }
-        }
-    }
-}
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    sieve();
-    another_sieve();
-    int n; cin >> n;
-    for(int i = 1; i <= n; ++i){
-        int x;
-        cin >> x;
-        cout << lpf[x] << " " << gpf[x] << " " << 0 <<
-            " " << 0 << " " << divcount[x] << " " << divsum[x] << "\n";
-    }
-    for(int i = 2; i <= 10; ++i){
-        cout << lpf[i] << "\n";
-    }
-    
+	// int tc; cin >> tc;
+	// while(tc--)
+		solve();
+
+	return 0;
 }
