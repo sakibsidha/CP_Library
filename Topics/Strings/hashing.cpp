66b490ceddd790948b6b45f61873328b766a3894
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-07-28 20:13:45 +0600

    Update hashing.cpp

--- Topics/Strings/hashing.cpp
+++ Topics/Strings/hashing.cpp
@@ -1,16 +1,12 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
+const int mod1 = 911382323, mod2 = 972663749, b1 = 137, b2 = 139;
+const int mxN = 5e6+10; // change
+int pow_b1[mxN], pow_b2[mxN], invpow_b1[mxN], invpow_b2[mxN];
 
-const ll mod1 = 911382323, mod2 = 972663749, b1 = 137, b2 = 139;
-const ll mxN = 1e6+10;
-ll pow_b1[mxN], pow_b2[mxN], invpow_b1[mxN], invpow_b2[mxN];
-
-ll power(ll a, ll b, ll mod){
-  ll ans = 1;
+int power(int a, int b, int mod){
+  int ans = 1;
   while(b){
-    if(b&1) ans = (ans * a) % mod;
-    a = (a * a) % mod;
+    if(b&1) ans = (ans * 1LL * a) % mod;
+    a = (a * 1LL * a) % mod;
     b >>= 1;
   }
   return ans;
@@ -19,73 +15,48 @@ ll power(ll a, ll b, ll mod){
 void pre(){
   pow_b1[0] = pow_b2[0] = 1;
   for(int i = 1; i < mxN; ++i){
-    pow_b1[i] = pow_b1[i-1] * b1 % mod1;
-    pow_b2[i] = pow_b2[i-1] * b2 % mod2;
+    pow_b1[i] = 1LL * pow_b1[i-1] * b1 % mod1;
+    pow_b2[i] = 1LL * pow_b2[i-1] * b2 % mod2;
   }
   invpow_b1[mxN-1] = power(pow_b1[mxN-1], mod1-2, mod1);
   invpow_b2[mxN-1] = power(pow_b2[mxN-1], mod2-2, mod2);
-	for(int i = mxN-2; i >= 0; --i){
-    invpow_b1[i] = (invpow_b1[i+1]*b1)%mod1;
-    invpow_b2[i] = (invpow_b2[i+1]*b2)%mod2;
+  for(int i = mxN-2; i >= 0; --i){
+    invpow_b1[i] = (1LL * invpow_b1[i+1] * b1)%mod1;
+    invpow_b2[i] = (1LL * invpow_b2[i+1] * b2)%mod2;
   }
 }
 
-pair<ll,ll> getHash(string& s){
-  ll hsh1 = 0, hsh2 = 0, n = s.size();
-  for(int i = 0; i < n; ++i) hsh1 = (hsh1 + s[i] * pow_b1[i] % mod1) % mod1;
-  for(int i = 0; i < n; ++i) hsh2 = (hsh2 + s[i] * pow_b2[i] % mod2) % mod2;
+pair<int,int> getHash(string& str){
+  int hsh1 = 0, hsh2 = 0, sz = str.size();
+  for(int i = 0; i < sz; ++i) hsh1 = (hsh1 + 1LL * str[i] * pow_b1[i] % mod1) % mod1;
+  for(int i = 0; i < sz; ++i) hsh2 = (hsh2 + 1LL * str[i] * pow_b2[i] % mod2) % mod2;
   return {hsh1, hsh2};
 }
 
-vector<pair<ll,ll>> getPrefixHash(string& s){
-  ll n = s.size();
-  vector<pair<ll,ll>> ret(n);
-  for(int i = 0; i < n; ++i){
-    if(!i){
-      ret[i].first =  (s[i] * pow_b1[i] % mod1) % mod1;
-      ret[i].second =  (s[i] * pow_b2[i] % mod2) % mod2;
+vector<pair<int,int>> getPrefixHash(string& str){
+  int sz = str.size();
+  vector<pair<int,int>> ret(sz);
+  for(int i = 0; i < sz; ++i){
+    if(i == 0){
+      ret[i].first = (1LL * str[i] * pow_b1[i] % mod1) % mod1;
+      ret[i].second = (1LL * str[i] * pow_b2[i] % mod2) % mod2;
     }
     else{
-      ret[i].first = (ret[i-1].first +  s[i] * pow_b1[i] % mod1) % mod1;
-      ret[i].second = (ret[i-1].second +  s[i] * pow_b2[i] % mod2) % mod2;
+      ret[i].first = (ret[i-1].first +  1LL * str[i] * pow_b1[i] % mod1) % mod1;
+      ret[i].second = (ret[i-1].second +  1LL * str[i] * pow_b2[i] % mod2) % mod2;
     }
   }
   return ret;
 }
 
-pair<ll,ll> getSubstrHash(ll l, ll r, vector<pair<ll,ll>>& pf_hash){
-  if(l == 0) return pf_hash[r];
+pair<int,int> getSubstrHash(int l, int r, vector<pair<int,int>>& prefixHash){
+  if(l == 0) return prefixHash[r];
   else{
-    pair<ll,ll> ans;
-    ans.first = (pf_hash[r].first - pf_hash[l-1].first + mod1) % mod1;
-    ans.first = (ans.first * invpow_b1[l]) % mod1;
-    ans.second = (pf_hash[r].second - pf_hash[l-1].second + mod2) % mod2;
-    ans.second = (ans.second * invpow_b2[l]) % mod2;
-    return ans;
+    pair<int,int> ret;
+    ret.first = (prefixHash[r].first - prefixHash[l-1].first + mod1) % mod1;
+    ret.first = (1LL * ret.first * invpow_b1[l]) % mod1;
+    ret.second = (prefixHash[r].second - prefixHash[l-1].second + mod2) % mod2;
+    ret.second = (1LL * ret.second * invpow_b2[l]) % mod2;
+    return ret;
   }
-} 
-
-void solve(){
-  // hash
-  string s; cin >> s;
-  auto [a, b] = getHash(s);
-  cout << a << " " << b << "\n";
-  
-  // substring hash
-  vector<pair<ll,ll>> pf_hash = getPrefixHash(s);
-  int l, r; cin >> l >> r;
-  auto [c, d] = getSubstrHash(l-1, r-1, pf_hash);
-  cout << c << " " << d << "\n";
-}
-
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  
-  pre();
-  int tc; cin >> tc;
-  while(tc--)
-    solve();
-
-  return 0;
 }
