0823f63034ac5c4678da181d68d1fe69cc58c3d7
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-08-10 13:34:08 +0600

    Update nCr.cpp

--- Topics/Math/nCr.cpp
+++ Topics/Math/nCr.cpp
@@ -7,7 +7,7 @@ const ll mxN = 2*1e6; // change
 const ll mod = 1e9+7; // change
 int fact[mxN], inv[mxN]; // fact and inverse fact array
 
-ll power(ll a, ll b){ ll ans = 1; while(b){ if(b&1) ans = (ans*a)%mod; a = (a*a)%mod; b >>= 1; } return ans; }
+ll power(ll a, ll b){ ll ans = 1; while(b){ if(b&1) ans = (ans*a)%mod; a = (a*a)%mod; b >>= 1LL; } return ans; }
 
 void preFact(){
 	fact[0] = inv[0] = 1;
