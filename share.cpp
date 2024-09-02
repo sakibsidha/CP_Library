c7630e1b57a166678c8edeb258a2f553cc769994
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-09-02 12:06:03 +0600

    Update share.cpp

--- share.cpp
+++ share.cpp
@@ -1,69 +1,45 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-const ll inf = 1e18;
+def avg(a, b):
+    c = (a + b)/2
+    return c
 
-ll f(ll i, ll j, vector<bool>& vis, vector<vector<ll>>& v, vector<vector<ll>>& dp){
-    if(dp[i][j] != -1) return dp[i][j];
-    dp[i][j] = inf;
-    for(int u: v[i]){
-        dp[i][j] = min(dp[i][j], 1 + f(u, j));
-    }
-    dp[i][j] = dp[j][i];
-    return dp[i][j];
-}
+a = float(input("Enter a number: "))
+b = float(input("Enter another number: "))
+av = avg(a, b)
+print("Their average is", av)
 
-void solve(){
-    ll n; cin >> n;
-    vector<vector<ll>> v(n+1);
-    vector<vector<ll>> dp(n+1, vector<ll>(n+1, -1));
-    cin.ignore();
-    for(int x = 1; x <= n; ++x){
-        string s;
-        getline(cin, s);
-        vector<int> a;
-        for(int i = 0; i < s.size(); ++i){
-            string t;
-            while(i < n and s[i] >= '0' and s[i] <= '9'){
-                t.push_back(s[i]);
-                ++i;
-            }
-            int num = stoi(t);
-            a.push_back(num);
-        }
-        for(int i = 0; i < a.size(); ++i){
-            v[i].push_back(a[i]);
-            a[i].push_back(v[i]);
-            dp[a[i]][v[i]] = 1;
-            dp[v[i]][a[i]] = 1;
-        }
-    }
-    map<ll,vector<ll>> mp;
-    for(int i = 1; i <= n; ++i){
-        for(int j = i+1; j <= n; ++j){
-            vector<bool> vis(n+1);
-            ll cnt = f(i, j, vis, v, dp);
-            mp[i].push_back(cnt);
-            mp[j].push_back(cnt);
-        }
-    }
-    vector<ll,ld> each;
-    for(int i = 1; i <= n; ++i){
-        ld sum = 0;
-        for(auto it: mp){
-            sum += it.second;
-        }
-        
-    }
-}
+lst = [2, 2, 3, 4, 5, 7, 8]
+print("Numbers in the list are: ", end = "")
+for i in lst:
+    if(i % 2 == 0):
+        print(i, end = " ")
+print()
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+n = int(input())
+for i in range(1, 11):
+    if i == 6:
+        continue
+    if i > 8:
+        break
+    mult = n * i
+    print(n, " x ", i, " = ", mult)
 
-    int tc; cin >> tc;
-    while(tc--)
-        solve();
+sum = 0
+for i in range (1, 11):
+    sum += i
+print("Sum of numbers from 1 to 10: ", sum)
+
+lst = [1, 2, 3, 4, 5]
+sum = 0
+odd = 0
+even = 0
+for i in lst:
+    if i % 2 == 0:
+        sum += i
+        even += 1
+    else:
+        odd += 1
     
-    return 0;
-}
+print("sum of even numbers in the list is: ", sum)
+print("Even count: ", even, "\nOdd count: ", odd)
+    
+    
