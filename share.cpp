d6a1341b82e63fb84d031c48855d6de57834f8db
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-09-02 12:16:25 +0600

    Update share.cpp

--- share.cpp
+++ share.cpp
@@ -1,45 +1,51 @@
-def avg(a, b):
-    c = (a + b)/2
-    return c
+# def avg(a, b):
+#     c = (a + b)/2
+#     return c
 
-a = float(input("Enter a number: "))
-b = float(input("Enter another number: "))
-av = avg(a, b)
-print("Their average is", av)
+# a = float(input("Enter a number: "))
+# b = float(input("Enter another number: "))
+# av = avg(a, b)
+# print("Their average is", av)
 
-lst = [2, 2, 3, 4, 5, 7, 8]
-print("Numbers in the list are: ", end = "")
-for i in lst:
-    if(i % 2 == 0):
-        print(i, end = " ")
+# lst = [2, 2, 3, 4, 5, 7, 8]
+# print("Numbers in the list are: ", end = "")
+# for i in lst:
+#     if(i % 2 == 0):
+#         print(i, end = " ")
+# print()
+
+# n = int(input())
+# for i in range(1, 11):
+#     if i == 6:
+#         continue
+#     if i > 8:
+#         break
+#     mult = n * i
+#     print(n, " x ", i, " = ", mult)
+
+# sum = 0
+# for i in range (1, 11):
+#     sum += i
+# print("Sum of numbers from 1 to 10: ", sum)
+
+# lst = [1, 2, 3, 4, 5]
+# sum = 0
+# odd = 0
+# even = 0
+# for i in lst:
+#     if i % 2 == 0:
+#         sum += i
+#         even += 1
+#     else:
+#         odd += 1
+    
+# print("sum of even numbers in the list is: ", sum)
+# print("Even count: ", even, "\nOdd count: ", odd)
+
+lst = [1, 2, 8, 5 , 4, 3]
+for i in range (len(lst)):
+    print(lst[i], end = " ")
 print()
 
-n = int(input())
-for i in range(1, 11):
-    if i == 6:
-        continue
-    if i > 8:
-        break
-    mult = n * i
-    print(n, " x ", i, " = ", mult)
-
-sum = 0
-for i in range (1, 11):
-    sum += i
-print("Sum of numbers from 1 to 10: ", sum)
-
-lst = [1, 2, 3, 4, 5]
-sum = 0
-odd = 0
-even = 0
-for i in lst:
-    if i % 2 == 0:
-        sum += i
-        even += 1
-    else:
-        odd += 1
-    
-print("sum of even numbers in the list is: ", sum)
-print("Even count: ", even, "\nOdd count: ", odd)
     
     
