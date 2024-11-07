fd14df2e37a963ef71e60e7e98b965d8321402c8
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2024-11-08 00:26:47 +0600

    Update share.cpp

--- share.cpp
+++ share.cpp
@@ -1,51 +1,84 @@
-# def avg(a, b):
-#     c = (a + b)/2
-#     return c
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
 
-# a = float(input("Enter a number: "))
-# b = float(input("Enter another number: "))
-# av = avg(a, b)
-# print("Their average is", av)
+vector<ll> tr, lazy;
 
-# lst = [2, 2, 3, 4, 5, 7, 8]
-# print("Numbers in the list are: ", end = "")
-# for i in lst:
-#     if(i % 2 == 0):
-#         print(i, end = " ")
-# print()
+void push(int nd, int s, int e) {
+  if (lazy[nd]) {
+    tr[nd] += (e - s + 1) * lazy[nd];
+    if (s != e) {
+      lazy[2*nd] += lazy[nd];
+      lazy[2*nd + 1] += lazy[nd];
+    }
+    lazy[nd] = 0;
+  }
+}
 
-# n = int(input())
-# for i in range(1, 11):
-#     if i == 6:
-#         continue
-#     if i > 8:
-#         break
-#     mult = n * i
-#     print(n, " x ", i, " = ", mult)
+ll query(int nd, int s, int e, int l, int r) {
+  push(nd, s, e);
+  if (s > r or e < l) return 0;
+  if (s >= l and e <= r) {
+    return tr[nd];
+  }
+  int mid = (s + e) >> 1;
+  return (query(2*nd, s, mid, l, r) + query(2*nd+1, mid + 1, e, l, r));
+}
 
-# sum = 0
-# for i in range (1, 11):
-#     sum += i
-# print("Sum of numbers from 1 to 10: ", sum)
+void update(int nd, int s, int e, int l, int r, int v) {
+  push(nd, s, e);
+  if (s > r or e < l) return;
+  if (s >= l and e <= r) {
+    lazy[nd] += v;
+    push(nd, s, e);
+    return;
+  }
+  int mid = (s + e) >> 1;
+  update(2*nd, s, mid, l, r, v);
+  update(2*nd+1, mid + 1, e, l, r, v);
+  tr[nd] = tr[2*nd] + tr[2*nd + 1];
+}
 
-# lst = [1, 2, 3, 4, 5]
-# sum = 0
-# odd = 0
-# even = 0
-# for i in lst:
-#     if i % 2 == 0:
-#         sum += i
-#         even += 1
-#     else:
-#         odd += 1
-    
-# print("sum of even numbers in the list is: ", sum)
-# print("Even count: ", even, "\nOdd count: ", odd)
+void solve() {
+  int n; cin >> n;
+  int q; cin >> q;
+  vector<ll> a(n);
+  // for (int i = 0; i < n; ++i) {
+  //   cin >> a[i];
+  // }
+  int sz = n;
+  while (__builtin_popcount(sz) != 1) {
+    sz++;
+    a.push_back(0);
+  }
+  tr.resize(2*sz);
+  lazy.resize(2*sz);
+  for (int i = 0; i < n; ++i) {
+    tr[sz + i] = a[i];
+  }
+  for (int i = sz - 1; i >= 1; --i) {
+    tr[i] = tr[2*i] + tr[2*i + 1];
+  }
+  while (q--) {
+    int t; cin >> t;
+    if (t == 2) {
+      int l, r; cin >> l >> r;
+      cout << query(1, 0, sz-1, l, r-1) << "\n";
+    }
+    else {
+      int l, r, v; cin >> l >> r >> v;
+      update(1, 0, n-1, l, r-1, v);
+    }
+  }
+}
 
-lst = [1, 2, 8, 5 , 4, 3]
-for i in range (len(lst)):
-    print(lst[i], end = " ")
-print()
+int32_t main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    
-    
+  // int tc; cin >> tc;
+  // while(tc--)
+    solve();
+
+  return 0;
+}
