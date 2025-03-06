399bcf0d07ed32f0fc9ac64c6d8a302d14494146
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2025-03-06 15:11:15 +0600

    Update share.cpp

--- share.cpp
+++ share.cpp
@@ -1,142 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+using ld = long double;
 
-void dfs(int u, vector<vector<int>>& adj, vector<bool>& vis) {
-  vis[u] = true;
-  for (auto v : adj[u]) {
-    if (!vis[v]) dfs(v, adj, vis);
-  }
-}
+#define PI acos(-1)
 
-vector<vector<int>> f(vector<pair<int,int>>& p, int x) {
-  int n = p.size() + 1;
-  vector<vector<int>> adj(n);
-  for (int i = 0; i < n-1; ++i) {
-    int u = p[i].first, v = p[i].second;
-    if (u != x and v != x){
-      adj[u].push_back(v);
-      adj[v].push_back(u);
-    }
-  }
-  return adj;
-}
+ld a, b, c;
 
-vector<vector<int>> f2(vector<pair<int,int>>& p, int x, int y) {
-  int n = p.size() + 1;
-  vector<vector<int>> adj(n);
-  for (int i = 0; i < n-1; ++i) {
-    int u = p[i].first, v = p[i].second;
-    if (u != x and v != x and u != y and v != y){
-      adj[u].push_back(v);
-      adj[v].push_back(u);
-    }
-  }
-  return adj;
+ld f(ld t) {
+  return a * t + b * sin (c * PI * t);
 }
 
 void solve() {
-  int n; cin >> n;
-  vector<vector<int>> adj(n);
-  vector<pair<int, int>> p;
-  vector<int> fq(n);
-  map<pair<int,int>, int> mp;
-  for (int i = 0; i < n-1; ++i) {
-    int u, v; cin >> u >> v, --u, --v;
-    adj[u].push_back(v);
-    adj[v].push_back(u);
-    p.push_back({u, v});
-    fq[u]++;
-    fq[v]++;
-    mp[{u, v}] = true;
-    mp[{v, u}] = true;
+  cin >> a >> b >> c;
+  ld l = 0.0, r = 1e9, ans = -1;
+  for (int i = 1; i <= 1000; ++i) {
+    ld mid = (l + r) / 2;
+    if (f(mid) >= 100.0) {
+      ans = mid;
+      r = mid;
+    } else l = mid;
   }
-  int two = 0, one = 0;
-  for (int i = 0; i < n; ++i) {
-    if (fq[i] == 2) two++;
-    if (fq[i] == 1) one++;
-  }
-  if (one == 2 and two == n - 2) {
-    if (n == 2) cout << 0 << "\n";
-    else if (n == 3) cout << 1 << "\n";
-    else if (n == 4) cout << 2 << "\n";
-    else cout << 3 << "\n";
-    return;
-  }
-  int mx = adj[0].size(), at = 0;
-  for (int i = 0; i < n; ++i) {
-    if (adj[i].size() > mx) {
-      mx = adj[i].size();
-      at = i;
-    }
-  }
-  int leaf[n]{};
-  for (int i = 0; i < n; ++i) {
-    if (adj[i].size() == 1) leaf[adj[i][0]]++; 
-  }
-  vector<pair<int,int>> can;
-  for (int i = 0; i < n; ++i) {
-    if (adj[i].size() == mx) {
-      can.push_back({leaf[i], i});
-    }
-  }
-  if (can.size() > 2 or (can.size() == 2 and mp[{can[0].second, can[1].second}] == false)) {
-    cout << (mx - 1) * 2 + 1 << "\n";
-    return;
-  }
-  sort(can.rbegin(), can.rend());
-  at = can[0].second;
-  vector<vector<int>> adj2 = f(p, at);
-  int mx2 = 0, at2 = -1;
-  for (int i = 0; i < n; ++i) {
-    if (i == at) continue;
-    mx2 = adj2[i].size();
-    at2 = i;
-    break;
-  }
-  for (int i = 0; i < n; ++i) {
-    if (i == at) continue;
-    if (adj2[i].size() > mx2) {
-      mx2 = adj2[i].size();
-      at2 = i;
-    }
-  }
-  int leaf2[n]{};
-  for (int i = 0; i < n; ++i) {
-    if (i == at) continue;
-    if (adj2[i].size() == 1) leaf2[adj2[i][0]]++; 
-  }
-  vector<pair<int,int>> can2;
-  for (int i = 0; i < n; ++i) {
-    if (i == at) continue;
-    if (adj2[i].size() == mx2) {
-      can2.push_back({leaf2[i], i});
-    }
-  }
-  sort(can2.rbegin(), can2.rend());
-  at2 = can2[0].second;
-  vector<vector<int>> adj3 = f2(p, at, at2);
-  int cnt = 0;
-  vector<bool> vis(n);
-  for (int i = 0; i < n; ++i) {
-    if (i == at or i == at2) continue;
-    else {
-      if (!vis[i]) {
-        dfs(i, adj3, vis);
-        cnt++;
-      }
-    }
-  }
-  cout << cnt << "\n";
+  cout << fixed << setprecision(15) << ans << "\n";
 }
 
 int32_t main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int tc; cin >> tc;
-  while (tc--)
+  int tc = 1;
+  // cin >> tc;
+  while (tc--) {
     solve();
+  }
 
   return 0;
 }
