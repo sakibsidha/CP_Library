e1699307bee3348090d7605b0a708ac2ff67acf1
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2025-03-13 17:26:01 +0600

    Update PBDS.cpp

--- Topics/Data Structure/PBDS.cpp
+++ Topics/Data Structure/PBDS.cpp
@@ -11,7 +11,7 @@ indexed_set<deta_type> st;
 
 usage: 
 st.find_by_order(x) :: returns iterator of the i-th element
-st.order_of_key(x) :: returns index of x (numbe of elements less than x)
+st.order_of_key(x) :: returns index of x (number of elements less than x)
 
 for multiset, replace less<T> with less_equal<T>
 
