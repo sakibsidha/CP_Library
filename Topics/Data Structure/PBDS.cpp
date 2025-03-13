58721995938eb56b380e3d6a18d308b3566a081d
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2025-03-13 17:25:41 +0600

    Update PBDS.cpp

--- Topics/Data Structure/PBDS.cpp
+++ Topics/Data Structure/PBDS.cpp
@@ -13,4 +13,6 @@ usage:
 st.find_by_order(x) :: returns iterator of the i-th element
 st.order_of_key(x) :: returns index of x (numbe of elements less than x)
 
+for multiset, replace less<T> with less_equal<T>
+
 */
