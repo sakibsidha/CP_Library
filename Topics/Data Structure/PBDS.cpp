d30ab900d8ff5f110e6f1f19bdffde7ec321e334
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2025-03-13 17:22:02 +0600

    Update PBDS.cpp

--- Topics/Data Structure/PBDS.cpp
+++ Topics/Data Structure/PBDS.cpp
@@ -3,3 +3,14 @@ using namespace __gnu_pbds;
 template<class T>
 using indexed_set = tree<T,null_type,less<T>,rb_tree_tag,
 tree_order_statistics_node_update>;
+
+/*
+
+declare:
+indexed_set<deta_type> st;
+
+usage: 
+st.find_by_order(x) :: returns iterator of the i-th element
+st.order_of_key(x) :: returns index of x (numbe of elements less than x)
+
+*/
