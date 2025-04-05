3d7d36317d6bec9ff1e712d710ef4ef9167d5a23
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2025-04-05 14:01:49 +0600

    Update share.cpp

--- share.cpp
+++ share.cpp
@@ -1,27 +1,6 @@
-echo -e "Welcome to OS Lab Calculator! \n"
+> output.txt
 
-for ((i = 1; ; ++i)); do
-	echo "Type of Operation:"
-	echo " 1. Addition"
-	echo " 2. Substraction"
-	echo " 3. Multiplication"
-	echo " 4. Division"
-	echo -e " 5. Close \n"
-	read n
-	if ((n == 5)); then
-		break
-	fi
-	echo -e "Enter 2 numbers: "
-	read a b
-	if ((n == 1)); then
-		echo -e "Result: $((a + b)) \n"
-	elif ((n == 2)); then 
-		echo -e "Result: $((a - b)) \n"
-	elif ((n == 3)); then
-		echo -e "Result: $((a * b)) \n"
-	elif ((n == 4)); then
-		echo -e "Result: $((a / b)) \n"
-	else
-		echo -e "Invalid Choice. \n"
-	fi	
-done	
+while read line; do
+	result=$(echo "$line" | bc)
+	echo "$result" >> output.txt
+done < input.txt
