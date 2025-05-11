0c53765b523c903dee02f18bb7ae0c49a033c4c5
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2025-05-11 13:48:42 +0600

    Update s.sh

--- my_setup/s.sh
+++ my_setup/s.sh
@@ -1,3 +1,7 @@
+g++ gen.cpp -o gen
+g++ brute.cpp -o brute
+g++ c.cpp -o c
+
 for((i = 1; ; ++i)); do
 	echo $i
 	./gen $i > in1
@@ -5,3 +9,10 @@ for((i = 1; ; ++i)); do
 	./brute < in1 > out2
 	diff -w out1 out2 || break
 done
+
+echo "case:"
+cat in1
+echo "expected:"
+cat out2
+echo "got:"
+cat out1
